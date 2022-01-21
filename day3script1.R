3c750a033b861b3025e1e07e5ca6a837fe1bf469
EShilland <e.shilland@ucl.ac.uk>
2022-01-21 13:33:42 +0000

    third tweak with a new x

--- day3script1.R
+++ day3script1.R
@@ -1,2 +1,12 @@
 #test for GitHub
 print("hello")
+
+#click box at top right in git window to tell git to track
+#then click commit with notes
+#it will then disappear from the git window
+#git 
+
+#i am adding more code
+
+# Make x
+x <- 1:10
